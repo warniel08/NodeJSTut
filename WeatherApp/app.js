f6d4b7244e570d71c1139a230baffc907c54690d
Warner Nielsen <bwn888@gmail.com>
2017-05-09 14:45:33 -0600

    Comment out all of the current code in app.js to test our call to the darksky weather api, write error handling

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,25 +1,40 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode');
+// const geocode = require('./geocode/geocode');
 
-const argv = yargs
-	.options({
-		a: {
-			demand: true,
-			alias: 'address',
-			describe: 'Address to fetch weather for',
-			string: true
-		}
-	})
-	.help()
-	.alias('help', 'h')
-	.argv;
+// const argv = yargs
+// 	.options({
+// 		a: {
+// 			demand: true,
+// 			alias: 'address',
+// 			describe: 'Address to fetch weather for',
+// 			string: true
+// 		}
+// 	})
+// 	.help()
+// 	.alias('help', 'h')
+// 	.argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-	if (errorMessage) {
-		console.log(errorMessage);
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+// 	if (errorMessage) {
+// 		console.log(errorMessage);
+// 	} else {
+// 		console.log(JSON.stringify(results, undefined, 2));
+// 	}
+// });
+
+// 35389068338d4cd61ec874e9271479b9
+// https://api.darksky.net/forecast/35389068338d4cd61ec874e9271479b9/LATITUDE,LONGITUDE
+
+const request = require('request');
+
+request({
+	url: 'https://api.darksky.net/forecast/35389068338d4cd61ec874e9271479b9/40.6999925,-112.0118669',
+	json: true
+}, (error, response, body) => {
+	if (!error && response.statusCode === 200) {
+		console.log(body.currently.temperature);
 	} else {
-		console.log(JSON.stringify(results, undefined, 2));
-	}
+		console.log('Unable to fetch weather');
+	}	
 });
-
