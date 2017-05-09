e45dc2b26089340b57e6705b9c9809c984aa66f3
Warner Nielsen <bwn888@gmail.com>
2017-05-09 14:13:27 -0600

    Create folder geocode with geocode.js file, transfer code from app.js to geocode.js, write new function to be called in app.js

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
 	.options({
@@ -14,19 +15,5 @@ const argv = yargs
 	.alias('help', 'h')
 	.argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
+geocode.geocodeAddress(argv.address);
 
-request({
-	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-	json: true
-}, (error, response, body) => {
-	if (error) {
-		console.log('Unable to connect to Google servers');
-	} else if (body.status === 'ZERO_RESULTS') {
-		console.log('Unable to find that address');
-	} else if (body.status === 'OK') {
-		console.log(`Address: ${body.results[0].formatted_address}`);
-		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-	}
-})
