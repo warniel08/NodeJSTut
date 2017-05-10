a8068023c09b316ef6661846514a4915f0520790
Warner Nielsen <bwn888@gmail.com>
2017-05-09 21:27:58 -0600

    Create root route, about route and bad route to learn express routing and creating a new page

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,5 +3,24 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-	
+	// res.send('<h1>Hello Express!</h1>');
+	res.send({
+		name: 'Warner',
+		likes: [
+			'Coding',
+			'Learning'
+		]
+	});
 });
+
+app.get('/about', (req, res) => {
+	res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Unable to handle request'
+	});
+});
+
+app.listen(3000);
