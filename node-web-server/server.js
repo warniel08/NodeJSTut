11fb2fdece5e150f1d6f3ce8a89248bfb593beea
Warner Nielsen <bwn888@gmail.com>
2017-05-11 15:15:57 -0600

    Create maintenance.hbs file, use server.js to render the maintenance page

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -6,7 +6,6 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname+ '/public'));
 
 app.use((req, res, next) => {
 	var now = new Date().toString();
@@ -20,6 +19,12 @@ app.use((req, res, next) => {
 	});
 	next();
 });
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname+ '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
 	return new Date().getFullYear()
