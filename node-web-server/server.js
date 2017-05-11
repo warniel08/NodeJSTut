4ea693d0c7ce9162c8c2b2fce294b06ade1a04cb
Warner Nielsen <bwn888@gmail.com>
2017-05-11 10:55:39 -0600

    Create views directory with about and home.hbs, add code to those to display html, create new renders for the / and about pages, save handlebars to app

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,22 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname+ '/public'));
 
 app.get('/', (req, res) => {
-	// res.send('<h1>Hello Express!</h1>');
-	res.send({
-		name: 'Warner',
-		likes: [
-			'Coding',
-			'Learning'
-		]
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		currentYear: new Date().getFullYear(),
+		welcomeMessage: "Hi there, welcome to my website"
 	});
 });
 
 app.get('/about', (req, res) => {
-	res.send('About Page');
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+		currentYear: new Date().getFullYear()
+	});
 });
 
 app.get('/bad', (req, res) => {
