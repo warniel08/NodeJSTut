ec0cf10785ed59d3dc64c62a6a769ddace2412b8
Warner Nielsen <bwn888@gmail.com>
2017-05-11 11:21:24 -0600

    Create partials folder with footer and header partials

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname+ '/public'));
 
