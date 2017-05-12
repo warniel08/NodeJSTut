2efefaf66bb3bd6760e71dcbd0d73193e1da551f
Warner Nielsen <bwn888@gmail.com>
2017-05-12 13:57:49 -0600

    Write asynchronous test for function

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -5,9 +5,15 @@ const utils = require('./utils');
 it('should add two numbers', () => {
 	var res = utils.add(33, 11);
 
-
 	expect(res).toBe(44).toBeA('number');
 });
+
+it('should async add two number', (done) => {
+	utils.asyncAdd(4, 3, (sum) => {
+		expect(sum).toBe(7).toBeA('number');
+		done();
+	})
+})
 	
 it('should sqaure a number', () => {
 	var res = utils.square(3);
