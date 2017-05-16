faad57e00caa37bc7d23b2a223b1cf59fd346f9d
Warner Nielsen <bwn888@gmail.com>
2017-05-16 14:21:32 -0600

    Add seed file for seeding test data, test users

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -4,22 +4,10 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-	_id: new ObjectID(),
-	text: 'First test todo'
-}, {
-	_id: new ObjectID(),
-	text: 'Second test todo',
-	completed: true,
-	completedAt: 333
-}];
-
-beforeEach((done) => {
-	Todo.remove({}).then(() => {
-		Todo.insertMany(todos);
-	}).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
 	it('should create a new todo', (done) => {
@@ -180,9 +168,5 @@ describe('PATCH /todos/:id', () => {
 				expect(res.body.todo.completedAt).toNotExist();
 			})
 			.end(done);
-		// grab id of second todo item
-		// update text, set completedd false
-		// 200
-		// text is changed, completed false, completedAt is null .toNotExist
 	});
 });
