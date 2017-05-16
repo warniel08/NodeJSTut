7013c9b1669b697b3bb98eebab9086a13dbe648f
Warner Nielsen <bwn888@gmail.com>
2017-05-16 01:16:35 -0600

    Setup separate test and development envs

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,6 +10,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -94,7 +97,7 @@ app.patch('/todos/:id', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('Started on port 3000');
 });
 
